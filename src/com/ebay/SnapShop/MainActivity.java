f7339b9bb77294b0048eb32b0ea14f0ace4e60a1
Balan <arbalan@paypal.com>
2014-07-26 10:58:54 -0700

    android app first commit

--- src/com/ebay/SnapShop/MainActivity.java
+++ src/com/ebay/SnapShop/MainActivity.java
@@ -1,15 +1,311 @@
 package com.ebay.SnapShop;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Base64;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 
-public class MainActivity extends Activity {
-    /**
-     * Called when the activity is first created.
-     */
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * Created by arbalan on 7/26/14.
+ */
+public class MainActivity extends Activity
+{
+    private static final int PICK_IMAGE = 1;
+    private static final int PICK_Camera_IMAGE = 2;
+    private ImageView imgView;
+    private Button upload,cancel;
+    private Bitmap bitmap;
+    private ProgressDialog dialog;
+    Uri imageUri;
+
+    MediaPlayer mp=new MediaPlayer();
+
+    /** Called when the activity is first created. */
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        imgView = (ImageView) findViewById(R.id.ImageView);
+        upload = (Button) findViewById(R.id.imguploadbtn);
+        cancel = (Button) findViewById(R.id.imgcancelbtn);
+
+        upload.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                if (bitmap == null) {
+                    Toast.makeText(getApplicationContext(),
+                            "Please select image", Toast.LENGTH_SHORT).show();
+                } else {
+                    dialog = ProgressDialog.show(MainActivity.this, "Uploading",
+                            "Please wait...", true);
+                    new ImageUploadTask().execute();
+                }
+            }
+        });
+
+        cancel.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                MainActivity.this.finish();
+            }
+        });
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_camera:
+                //define the file-name to save photo taken by Camera activity
+                String fileName = "new-photo-name.jpg";
+                //create parameters for Intent with filename
+                ContentValues values = new ContentValues();
+                values.put(MediaStore.Images.Media.TITLE, fileName);
+                values.put(MediaStore.Images.Media.DESCRIPTION,"Image capture by camera");
+                //imageUri is the current activity attribute, define and save it for later usage (also in onSaveInstanceState)
+                imageUri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+                //create new Intent
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+                intent.putExtra(MediaStore.EXTRA_VIDEO_QUALITY, 1);
+                startActivityForResult(intent, PICK_Camera_IMAGE);
+                return true;
+
+            case R.id.menu_gallery:
+                try {
+                    Intent gintent = new Intent();
+                    gintent.setType("image/*");
+                    gintent.setAction(Intent.ACTION_GET_CONTENT);
+                    startActivityForResult(
+                            Intent.createChooser(gintent, "Select Picture"),
+                            PICK_IMAGE);
+                } catch (Exception e) {
+                    Toast.makeText(getApplicationContext(),
+                            e.getMessage(),
+                            Toast.LENGTH_LONG).show();
+                    Log.e(e.getClass().getName(), e.getMessage(), e);
+                }
+                return true;
+        }
+        return false;
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Uri selectedImageUri = null;
+        String filePath = null;
+        switch (requestCode) {
+            case PICK_IMAGE:
+                if (resultCode == Activity.RESULT_OK) {
+                    selectedImageUri = data.getData();
+                }
+                break;
+            case PICK_Camera_IMAGE:
+                if (resultCode == RESULT_OK) {
+                    //use imageUri here to access the image
+                    selectedImageUri = imageUri;
+                } else if (resultCode == RESULT_CANCELED) {
+                    Toast.makeText(this, "Picture was not taken", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, "Picture was not taken", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+
+        if(selectedImageUri != null){
+            try {
+                // OI FILE Manager
+                String filemanagerstring = selectedImageUri.getPath();
+
+                // MEDIA GALLERY
+                String selectedImagePath = getPath(selectedImageUri);
+
+                if (selectedImagePath != null) {
+                    filePath = selectedImagePath;
+                } else if (filemanagerstring != null) {
+                    filePath = filemanagerstring;
+                } else {
+                    Toast.makeText(getApplicationContext(), "Unknown path",
+                            Toast.LENGTH_LONG).show();
+                    Log.e("Bitmap", "Unknown path");
+                }
+
+                if (filePath != null) {
+                    decodeFile(filePath);
+                } else {
+                    bitmap = null;
+                }
+            } catch (Exception e) {
+                Toast.makeText(getApplicationContext(), "Internal error",
+                        Toast.LENGTH_LONG).show();
+                Log.e(e.getClass().getName(), e.getMessage(), e);
+            }
+        }
+
+    }
+
+    class ImageUploadTask extends AsyncTask<Void, Void,String> {
+    @SuppressWarnings("unused")
+    @Override
+    protected String doInBackground(Void... unsued) {
+        InputStream is;
+        BitmapFactory.Options bfo;
+        Bitmap bitmapOrg;
+        ByteArrayOutputStream bao ;
+
+        bfo = new BitmapFactory.Options();
+        bfo.inSampleSize = 2;
+//bitmapOrg = BitmapFactory.decodeFile(Environment.getExternalStorageDirectory() + "/" + customImage, bfo);
+
+        bao = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 90, bao);
+        byte [] ba = bao.toByteArray();
+        String ba1 = Base64.encodeToString(ba, Base64.DEFAULT);
+        ArrayList nameValuePairs = new ArrayList();
+        nameValuePairs.add(new BasicNameValuePair("image",ba1));
+        nameValuePairs.add(new BasicNameValuePair("cmd","image_android"));
+        Log.v("log_tag", System.currentTimeMillis()+".jpg");
+        try{
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpPost httppost = new HttpPost("http://uploads.im/api");
+            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+            HttpResponse response = httpclient.execute(httppost);
+
+            Log.i("SnapShop","************ Response :"+response.toString());
+            HttpEntity entity = response.getEntity();
+            is = entity.getContent();
+            getStringContent(is,response);
+            Log.v("log_tag", "In the try Loop" );
+        }catch(Exception e){
+            Log.v("log_tag", "Error in http connection "+e.toString());
+        }
+        return "Success";
+// (null);
+    }
+
+    private String getStringContent(InputStream ips,HttpResponse response) throws Exception
+    {
+        BufferedReader buf = new BufferedReader(new InputStreamReader(ips,"UTF-8"));
+        if(response.getStatusLine().getStatusCode()!= HttpStatus.SC_OK)
+        {
+            throw new Exception(response.getStatusLine().getReasonPhrase());
+        }
+        StringBuilder sb = new StringBuilder();
+        String s;
+        while(true )
+        {
+            s = buf.readLine();
+            if(s==null || s.length()==0)
+                break;
+            sb.append(s);
+
+        }
+        buf.close();
+        ips.close();
+        Log.i("SnapShop","************ Response 1 :"+sb.toString());
+        return sb.toString();
+    }
+
+    @Override
+    protected void onProgressUpdate(Void... unsued) {
+
+    }
+
+    @Override
+    protected void onPostExecute(String sResponse) {
+        try {
+            if (dialog.isShowing())
+                dialog.dismiss();
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(),
+                    e.getMessage(),
+                    Toast.LENGTH_LONG).show();
+            Log.e(e.getClass().getName(), e.getMessage(), e);
+        }
+    }
+
+}
+
+    public String getPath(Uri uri) {
+        String[] projection = { MediaStore.Images.Media.DATA };
+        Cursor cursor = managedQuery(uri, projection, null, null, null);
+        if (cursor != null) {
+            // HERE YOU WILL GET A NULLPOINTER IF CURSOR IS NULL
+            // THIS CAN BE, IF YOU USED OI FILE MANAGER FOR PICKING THE MEDIA
+            int column_index = cursor
+                    .getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToFirst();
+            return cursor.getString(column_index);
+        } else
+            return null;
+    }
+
+    public void decodeFile(String filePath) {
+        // Decode image size
+        BitmapFactory.Options o = new BitmapFactory.Options();
+        o.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(filePath, o);
+
+        // The new size we want to scale to
+        final int REQUIRED_SIZE = 1024;
+
+        // Find the correct scale value. It should be the power of 2.
+        int width_tmp = o.outWidth, height_tmp = o.outHeight;
+        int scale = 1;
+        while (true) {
+            if (width_tmp < REQUIRED_SIZE && height_tmp < REQUIRED_SIZE)
+                break;
+            width_tmp /= 2;
+            height_tmp /= 2;
+            scale *= 2;
+        }
+
+        // Decode with inSampleSize
+        BitmapFactory.Options o2 = new BitmapFactory.Options();
+        o2.inSampleSize = scale;
+        bitmap = BitmapFactory.decodeFile(filePath, o2);
+
+        imgView.setImageBitmap(bitmap);
+
+    }
+
 }
